ba24e693b958c53b23f3e20fba0233e213c1c87a
Supersmf <Supersmf@mail.ru>
2017-09-28 00:29:22 +0300

    additional_5/feature: done 17 tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ let stack = [];
+	for (var i = 0; i < str.length; i++) {
+		for (var j = 0; j < bracketsConfig.length; j++) {
+			if(str[i] === bracketsConfig[j][0]) stack.push(str[i]);
+			if (str[i] === bracketsConfig[j][1]) {
+				if (stack.pop() != bracketsConfig[j][0]) { 
+					return false }
+			}
+		}
+	}
+	if(stack.length) return false;
+	return true;
 }
