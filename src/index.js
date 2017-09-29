1ce38d83cce76aa33c6e93767e1b104a6f62ec06
Supersmf <Supersmf@mail.ru>
2017-09-29 21:19:10 +0300

    additional_5/feature: done 100% of tests

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
- let stack = [];
+	if (str.length%2) return false;
+ 	let stack = [];
 	for (var i = 0; i < str.length; i++) {
 		for (var j = 0; j < bracketsConfig.length; j++) {
-			if(str[i] === bracketsConfig[j][0]) stack.push(str[i]);
-			if (str[i] === bracketsConfig[j][1]) {
-				if (stack.pop() != bracketsConfig[j][0]) { 
-					return false }
-			}
+			if (str[i] === bracketsConfig[j][1] && stack[stack.length - 1] === bracketsConfig[j][0]){
+				stack.pop();
+			}else {
+				if (str[i] === bracketsConfig[j][0]){
+					stack.push(str[i]);
+				}
+            }
 		}
 	}
-	if(stack.length) return false;
-	return true;
+	return stack.length?false:true;
 }
